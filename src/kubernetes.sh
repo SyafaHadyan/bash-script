e89f0f743f74637d482cd57465714b2e2c13061c
Syafa Hadyan Rasendriya <syafa@syafahadyan.com>
2025-10-07 14:56:08 +0700

    update

--- src/kubernetes.sh
+++ src/kubernetes.sh
@@ -33,6 +33,9 @@ sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo systemctl enable --now kubelet
 
+echo ""
 echo "===================="
+echo ""
 echo "please log out and log back in"
+echo ""
 echo "===================="
