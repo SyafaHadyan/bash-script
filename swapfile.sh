47c89a2c38872b8dbd160177530bba34fc2a1478
Syafa Hadyan Rasendriya <syafa@syafahadyan.com>
2026-03-29 12:22:48 +0700

    update

---
+++ swapfile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+sudo dd if=/dev/zero of=/swapfile bs=1M count=4096
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+echo "/swapfile none swap sw,pri=1 0 0" | sudo tee -a /etc/fstab
